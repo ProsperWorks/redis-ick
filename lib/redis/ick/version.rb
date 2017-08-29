3515872beec4477d40ccdb0bed6b918459f51bd0
jhwillett <jhw@prosperworks.com>
2017-08-29 14:17:35 -0700

    Bump version to 0.0.3.

--- lib/redis/ick/version.rb
+++ lib/redis/ick/version.rb
@@ -1,17 +1,21 @@
 class Redis
   class Ick
     #
-    # Version plan:
+    # Version plan/history:
     #
-    # 0.0.1 - still in Prosperworks/ALI/vendor/gems/redis-ick
+    # 0.0.1 - Still in Prosperworks/ALI/vendor/gems/redis-ick.
     #
-    # 0.0.2 - broke out into Prosperworks/redis-ick
+    # 0.0.2 - Broke out into Prosperworks/redis-ick, make public.
     #
-    # 0.1.0 - big README.md and Rdoc update, open repo
+    # 0.0.3 - Got .travis.yml working with a live redis-server, some
+    #         cleanup, runtime dependency on redis-script_manager for
+    #         Ick._eval.
     #
-    # 0.2.0 - solicit and incorporate initial feedback from select
-    #         beta external users
+    # 0.1.0 - (future) Big README.md and Rdoc update, solicit feedback
+    #         from select external beta users.
     #
-    VERSION = "0.0.2" # broken into standalone repo, not polished
+    # 0.2.0 - (future) Incorporate feedback, announce.
+    #
+    VERSION = '0.0.3'
   end
 end
